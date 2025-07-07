26ed2c765ab3abda5dacb35f00612f1ff8400a43
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-07 10:52:50 +0530

    User and Rating Models Created

--- core/models.py
+++ core/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 '''Restaurant Management System'''
 
+# 1) Restaurant Model
 class Restaurant(models.Model):
     name=models.CharField(max_length=100)
     website=models.URLField(default='')
@@ -12,3 +14,16 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+    
+# 2) Built-in User Model
+
+# 3) Rating  Model
+class Rating(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    restaurant=models.ForeignKey(Restaurant,on_delete=models.CASCADE)
+    rating=models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"Rating : {self.rating}"
+
+
