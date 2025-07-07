45b5a20117e6c651204b772104e8d94cfdaf8ee1
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-07 10:37:42 +0530

    Created a Restaurant Model For Restaurant Management System

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+'''Restaurant Management System'''
+
+class Restaurant(models.Model):
+    name=models.CharField(max_length=100)
+    website=models.URLField(default='')
+    date_opened=models.DateField()
+    latitude=models.FloatField()
+    longtitude=models.FloatField()
+
+    def __str__(self):
+        return self.name
