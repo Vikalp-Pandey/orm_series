10debd783505bc3ae79d66057ac10a21776b7591
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-07 13:31:03 +0530

    Registering the models in Admin Site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from core.models import Rating, Restaurant, Sale
+
 # Register your models here.
+admin.site.register(Restaurant)
+admin.site.register(Rating)
+admin.site.register(Sale)
