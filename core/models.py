aad1f7c6f5142624b0e35c7bb42cb28167964400
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-07 13:13:46 +0530

    New Column restaurant_type in Restaurant Model is Added

--- core/models.py
+++ core/models.py
@@ -5,19 +5,33 @@ from django.contrib.auth.models import User
 '''Restaurant Management System'''
 
 # 1) Restaurant Model
-class Restaurant(models.Model):
+class Restaurant(models.Model):   
+    '''Restaurant Class is Inhereting form django.db.models.Model'''
+    class TypeChoices(models.TextChoices):
+        INDIAN="IN" "Indian"
+        CHINESE="CH" "Chinese"
+        ITALIAN="IT" "Italian"
+        GREEK="GR" "Greek"
+        MEXICAN="Mx" "Mexican"
+        FASTFOOD="FF" "Fastfood"
+        OTHERS="OT" "Others"
+
+
     name=models.CharField(max_length=100)
     website=models.URLField(default='')
     date_opened=models.DateField()
     latitude=models.FloatField()
     longtitude=models.FloatField()
+    Restaurant_type=models.CharField(max_length=2, choices=TypeChoices.choices)
 
     def __str__(self):
         return self.name
-    
+        # Human-readable representation of a Restaurant object
+
+
 # 2) Built-in User Model
 
-# 3) Rating  Model
+# 3) Rating  Model: Rating should be done by a User defined in users Table and on Rstaurant defined in Restaurant Table.
 class Rating(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     restaurant=models.ForeignKey(Restaurant,on_delete=models.CASCADE)
@@ -25,5 +39,6 @@ class Rating(models.Model):
 
     def __str__(self):
         return f"Rating : {self.rating}"
+    # Human-readable representation of a Rating object
 
 
