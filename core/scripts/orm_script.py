83d790ee2be91d5a4518cc1d9612dba2fac04fc4
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-10 17:30:51 +0530

    1st Instance of Restaurant Created

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from core.models import Restaurant, Rating, Sale
 
 
@@ -7,3 +8,18 @@ def run():
 # Use the folloeing command to run this script:
 # python manage.py  runscript orm_script 
 
+# Creating a Record in Database by (Instatiating and Saving a model object)
+# We can use this same method to update as many parameters in a record.
+def run():
+    # M-1
+    restaurant=Restaurant()
+    restaurant.name="The Great Indian Restaurant"
+    restaurant.website="https://www.thegreatindianrestaurant.com"
+    restaurant.date_opened=timezone.now()
+    restaurant.latitude=28.7041
+    restaurant.longtitude=77.1025
+    restaurant.restaurant_type=Restaurant.TypeChoices.INDIAN
+    restaurant.save()  # Save the object to the database
+
+
+    
