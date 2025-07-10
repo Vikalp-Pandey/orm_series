12d97615d8e15e9eea973ee8a80ab43966e3acb0
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-10 18:02:26 +0530

    Querying the Database

--- core/scripts/orm_script.py
+++ core/scripts/orm_script.py
@@ -1,3 +1,4 @@
+from django.db import connection
 from django.utils import timezone
 from core.models import Restaurant, Rating, Sale
 
@@ -8,18 +9,49 @@ def run():
 # Use the folloeing command to run this script:
 # python manage.py  runscript orm_script 
 
-# Creating a Record in Database by (Instatiating and Saving a model object)
+# 1) Creating a Record in Database by (Instatiating and Saving a model object)
 # We can use this same method to update as many parameters in a record.
-def run():
-    # M-1
-    restaurant=Restaurant()
-    restaurant.name="The Great Indian Restaurant"
-    restaurant.website="https://www.thegreatindianrestaurant.com"
-    restaurant.date_opened=timezone.now()
-    restaurant.latitude=28.7041
-    restaurant.longtitude=77.1025
-    restaurant.restaurant_type=Restaurant.TypeChoices.INDIAN
-    restaurant.save()  # Save the object to the database
 
+# def run():
+#     # M-1
+#     restaurant=Restaurant()
+#     restaurant.name="The Great Indian Restaurant"
+#     restaurant.website="https://www.thegreatindianrestaurant.com"
+#     restaurant.date_opened=timezone.now()
+#     restaurant.latitude=28.7041
+#     restaurant.longtitude=77.1025
+#     restaurant.restaurant_type=Restaurant.TypeChoices.INDIAN
+#     restaurant.save()  # Save the object to the database
+
+def run():    
+    # M-2
+    restaurant = Restaurant.objects.create(
+        name="The Great Chinese Restaurant",
+        website="https://www.thegreatchineserestaurant.com",
+        date_opened=timezone.now(),
+        latitude=28.7041,
+        longtitude=77.1025,
+        restaurant_type=Restaurant.TypeChoices.CHINESE
+    )
+    print(restaurant)  # Print the created restaurant object
+    print(connection.queries)  # Print the SQL queries executed for the above operation
+
+# Querying the Database with ORM
+def run():
+    
+    # Fetching all restaurants
+    restaurants = Restaurant.objects.all()
+    print(restaurants)
+
+    # Fetching the first restaurant
+    first_restaurant = Restaurant.objects.first()
+
+    # Indexing into django querysets
+    first_restaurant=Restaurant.objects.all()[0]
+    
+
+    print(first_restaurant)
+    
+    
 
     
