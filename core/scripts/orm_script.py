8950e4c56b4f828fdd8871828c2bad2f939b7837
Vikalp-Pandey <vikalp.pandey2004@gmail.com>
2025-07-10 17:17:44 +0530

    Django ORM Scripts Created

---
+++ core/scripts/orm_script.py
@@ -0,0 +1,9 @@
+from core.models import Restaurant, Rating, Sale
+
+
+def run():
+    print("Hello world from orm_script.py!")
+
+# Use the folloeing command to run this script:
+# python manage.py  runscript orm_script 
+
